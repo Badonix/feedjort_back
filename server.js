9b46b88ac5367449f266226abc1f57eacd942bad
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2023-05-09 23:47:19 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const app = express();
+const cors = require("cors");
+const server = http.createServer(app);
+const { v4 } = require("uuid");
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+let users = [];
+app.use(cors());
+io.on("connection", (socket) => {
+  console.log("A user has connected");
+
+  socket.on("click", (data) => {
+    let isUser;
+    const id = data.id;
+    const calorie = data.calorie;
+    const nickname = data.nickname;
+    users.forEach((el) => {
+      if (el.id == id) {
+        isUser = true;
+      } else {
+        isUser = false;
+      }
+    });
+    if (!isUser) {
+      users = [...users, { nickname, id, calories: 0 }];
+    }
+
+    users = users.map((el) => {
+      if (el.id == id) {
+        el.calories += calorie;
+        return el;
+      } else {
+        return el;
+      }
+    });
+    console.log("==================");
+    console.log(users);
+    console.log("==================");
+  });
+
+  socket.on("get_calories", (data) => {
+    const { id } = data;
+    const user = users.find((el) => el.id == id);
+    socket.emit("calories", {
+      calories: user?.calories || 0,
+    });
+  });
+
+  socket.on("register", (data) => {
+    const id = v4();
+    socket.emit("get_id", {
+      id,
+    });
+    users = [...users, { nickname: data.nickname, id, calories: 0 }];
+
+    console.log(users);
+  });
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
+  });
+});
+
+app.get("/leaderboard", (req, res) => {
+  const sortedLeaderboard = users
+    .sort((a, b) => b.calories - a.calories)
+    .slice(0, 50);
+
+  res.json(sortedLeaderboard);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
